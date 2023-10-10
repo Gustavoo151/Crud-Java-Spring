c027bc96f5e834fb64c25a80a391771159cee735
gustavoo151 <josegustavooliveira121@gmail.com>
2023-10-10 00:28:36 -0300

    FindAll Terminado

--- src/main/java/com/gustavo/controller/CourseController.java
+++ src/main/java/com/gustavo/controller/CourseController.java
@@ -4,7 +4,6 @@ import com.gustavo.model.Course;
 import com.gustavo.repository.CourseRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
